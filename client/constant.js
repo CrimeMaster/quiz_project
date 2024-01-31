168fc48ff172c52708352ac9af0ce4702754c0ff
CrimeMaster <gtirkey300@gmail.com>
2024-01-31 22:48:11 +0530

    first commit

---
+++ client/constant.js
@@ -0,0 +1,2 @@
+export const URL = "http://localhost:4000/api/v1";
+export const REACT_APP_BASE_URL = `${window.location.origin}`;
---
+++ client/src/components/Dashboard/HeadingCard/HeadingCard.jsx
@@ -0,0 +1,13 @@
+import styles from "./headingCard.module.css";
+
+const HeadingCard = ({ text, color, counts }) => {
+  const fontColor = { color: color };
+  return (
+    <div style={fontColor} className={styles.card}>
+      <span className={styles.numericValue}> {counts}</span>
+      <span className={styles.textValue}>{text}</span>
+    </div>
+  );
+};
+
+export default HeadingCard;
